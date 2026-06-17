e6cf25948ce52d591c743876776d13fd49192110
Chopper-Tony <hi.hello.bonjour.ciao@gmail.com>
2026-06-17 20:48:33 +0900

    Solve D009 exercises

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ import numpy as np
 #
 # OpenCV 함수를 활용하세요.
 def convert_to_gray(image):
-    raise NotImplementedError
-
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # 문제 2.
 #
@@ -22,7 +21,7 @@ def resize_image(
     width,
     height,
 ):
-    raise NotImplementedError
+    return cv2.resize(image, (width, height))
 
 
 # 문제 3.
@@ -33,12 +32,15 @@ def resize_image(
 # Binary 이미지를 생성해야 합니다.
 #
 # 반환값은 Binary Image 입니다.
-def apply_threshold(
-    gray_image,
-    threshold_value,
-):
-    raise NotImplementedError
-
+def apply_threshold(gray_image, threshold_value):
+    _, binary_image = cv2.threshold(
+        gray_image,
+        threshold_value,
+        255,
+        cv2.THRESH_BINARY
+    )
+    return binary_image
+    
 
 # 문제 4.
 #
@@ -53,7 +55,12 @@ def apply_threshold(
 #
 # 조건에 맞는 픽셀 개수를 반환하세요.
 def count_red_pixels(image):
-    raise NotImplementedError
+    b = image[:, :, 0]
+    g = image[:, :, 1]
+    r = image[:, :, 2]
+
+    red_mask = (r > 200) & (g < 50) & (b < 50)
+    return int(np.sum(red_mask))
 
 
 # 문제 5.
@@ -68,4 +75,12 @@ def count_red_pixels(image):
 #
 # 객체가 없으면 None 반환
 def find_object_center(mask):
-    raise NotImplementedError
+    ys, xs = np.where(mask == 255)
+
+    if len(xs) == 0:
+        return None
+    
+    cx = int(np.mean(xs))
+    cy = int(np.mean(ys))
+
+    return (cx, cy)
